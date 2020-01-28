ddd072ffb50687361f5a3a8147189341b9c8e27d
Amir Armooey <amir@armooey.ir>
2020-01-28 19:26:27 +0330

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float speed = 10;
+    public float roatationSpeed = 20;
+
+    public void LookAt()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, roatationSpeed * Time.deltaTime);
+    }
+
+    public void MoveTo()
+    {
+        Vector3 position = target.position + target.forward * offset.z
+                                           + target.right * offset.x
+                                           + target.up * offset.y;
+        transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+    }
+
+    public void FixedUpdate()
+    {
+        LookAt();
+        MoveTo();
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalInput, verticalInput;
+    private float steeringAngle;
+    public float maxSteeringAngle = 30;
+    public float torque = 60;
+    public WheelCollider frontRightWheel, frontLeftWheel, rearRightWheel, rearLeftWheel;
+    public Transform frontRightTransform, frontLeftTransform, rearRightTransfrom, rearLeftTransform;
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Accelerate()
+    {
+        frontLeftWheel.motorTorque = torque * verticalInput;
+        frontRightWheel.motorTorque = torque * verticalInput;
+    }
+
+    private void Steer()
+    {
+        steeringAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheel.steerAngle = steeringAngle;
+        frontRightWheel.steerAngle = steeringAngle;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontLeftWheel, frontLeftTransform);
+        UpdateWheelPose(frontRightWheel, frontRightTransform);
+        UpdateWheelPose(rearLeftWheel,rearLeftTransform);
+        UpdateWheelPose(rearRightWheel, rearRightTransfrom);
+    }
+
+    private void UpdateWheelPose(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos = wheelTransform.position;
+        Quaternion rot = wheelTransform.rotation;
+        wheelCollider.GetWorldPose(out pos,out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+}
