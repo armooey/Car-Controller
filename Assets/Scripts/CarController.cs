ab2861b42e71efe81937ba40c4779b3989ead0c8
Amir Armooey <amir@armooey.ir>
2020-01-29 16:03:32 +0330

    Revert "Updated CarController.cs"
    This reverts commit 0eb33c6ec2d7577bac32f693d08b283a91006a99.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,35 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public enum Drivetrain {FWD,
-        RWD,
-        FourWD};
-    
     private float horizontalInput, verticalInput;
     private float steeringAngle;
-    
-    
-    [Header("Car Settings")]
-    public Drivetrain drivetrain = Drivetrain.FWD;
     public float maxSteeringAngle = 30;
     public float torque = 60;
-
-    [Header("Wheel Colliders")] 
-    public WheelCollider frontRightWheel;
-    public WheelCollider frontLeftWheel;
-    public WheelCollider rearRightWheel;
-    public WheelCollider rearLeftWheel;
-
-    [Header("Wheels")] 
-    public Transform frontRightTransform;
-    public Transform frontLeftTransform;
-    public Transform rearRightTransfrom;
-    public Transform rearLeftTransform;
+    public WheelCollider frontRightWheel, frontLeftWheel, rearRightWheel, rearLeftWheel;
+    public Transform frontRightTransform, frontLeftTransform, rearRightTransfrom, rearLeftTransform;
 
     private void GetInput()
     {
@@ -39,23 +20,8 @@ public class CarController : MonoBehaviour
 
     private void Accelerate()
     {
-        switch (drivetrain)
-        {
-            case Drivetrain.FWD:
-                frontLeftWheel.motorTorque = torque * verticalInput;
-                frontRightWheel.motorTorque = torque * verticalInput;
-                break;
-            case Drivetrain.RWD:
-                rearLeftWheel.motorTorque = torque * verticalInput;
-                rearRightWheel.motorTorque = torque * verticalInput;
-                break;
-            case Drivetrain.FourWD:
-                frontLeftWheel.motorTorque = torque * verticalInput;
-                frontRightWheel.motorTorque = torque * verticalInput;
-                rearLeftWheel.motorTorque = torque * verticalInput;
-                rearRightWheel.motorTorque = torque * verticalInput;
-                break;
-        }
+        frontLeftWheel.motorTorque = torque * verticalInput;
+        frontRightWheel.motorTorque = torque * verticalInput;
     }
 
     private void Steer()
